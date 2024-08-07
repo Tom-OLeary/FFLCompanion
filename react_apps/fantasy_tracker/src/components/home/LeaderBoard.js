389d2d1acc2c0e7785caa573843fe53a9ddc686f
Tom O'Leary <tr.oleary@outlook.com>
2024-08-07 16:26:58 -0400

    add active toggle to leaderboard

--- react_apps/fantasy_tracker/src/components/home/LeaderBoard.js
+++ react_apps/fantasy_tracker/src/components/home/LeaderBoard.js
@@ -15,6 +15,11 @@ export default function LeaderBoard(props) {
     const [data, setData] = useState([]);
     const [rankings, setRankings] = useState([]);
     const [firstPlace, setFirstPlace] = useState([]);
+    const [secondPlace, setSecondPlace] = useState([]);
+    const [isActive, setIsActive] = useState([]);
+    const [allData, setAllData] = useState([]);
+    const [dataRow, setDataRow] = useState("titles");
+
     let endpoint = props.url + "leaders/"
 
     useEffect(() => {
@@ -23,53 +28,112 @@ export default function LeaderBoard(props) {
             .then(json => {
                 console.log(json);
                 setData(json);
-                setRankings(json["titles"])
-                setFirstPlace(json["titles"][0])
+                setAllData(json);
+                setRankings(json["titles"]);
+                setFirstPlace(json["titles"][0]);
+                setSecondPlace(json["titles"][1]);
+                // setMenuTab("Titles");
             })
             .catch(err => console.log(err));
     }, []);
+
+    let activeOnly = {
+        titles: [],
+        points: [],
+        wins: [],
+        points_max: [],
+        wins_max: [],
+        ppg: [],
+        playoffs: [],
+        finals: [],
+        playoff_rate: [],
+    }
+    Object.keys(data).forEach((key) => {
+        let rows = data[key];
+        for (const r in rows) {
+            if (rows[r]["is_active"]) {activeOnly[key].push(rows[r])}
+        }
+    })
+
+    const setActive = () => {
+        // gotta be a better way to do this
+        // issue data not loading without double click
+        if (isActive) {
+            setData(activeOnly);
+            setRankings(activeOnly[dataRow]);
+            setFirstPlace(activeOnly[dataRow][0]);
+            setSecondPlace(activeOnly[dataRow][1]);
+        } else {
+            setData(allData);
+            setRankings(allData[dataRow]);
+            setFirstPlace(allData[dataRow][0]);
+            setSecondPlace(allData[dataRow][1]);
+        }
+        // setValue(0);
+        setIsActive(!isActive);
+    }
 
     const handleClick = (tab) => {
         switch (tab) {
             case "Titles":
                 setRankings(data["titles"]);
                 setFirstPlace(data["titles"][0]);
+                setSecondPlace(data["titles"][1]);
+                setDataRow("titles");
                 break;
             case "Points/Yr":
                 setRankings(data["points"]);
                 setFirstPlace(data["points"][0]);
+                setSecondPlace(data["points"][1]);
+                setDataRow("points");
                 break;
             case "Wins/Yr":
                 setRankings(data["wins"]);
                 setFirstPlace(data["wins"][0]);
+                setSecondPlace(data["wins"][1]);
+                setDataRow("wins");
                 break;
             case "Points/Max":
                 setRankings(data["points_max"]);
                 setFirstPlace(data["points_max"][0]);
+                setSecondPlace(data["points_max"][1]);
+                setDataRow("points_max");
                 break;
             case "Wins/Max":
                 setRankings(data["wins_max"]);
                 setFirstPlace(data["wins_max"][0]);
+                setSecondPlace(data["wins_max"][1]);
+                setDataRow("wins_max");
                 break;
             case "PPG":
                 setRankings(data["ppg"]);
                 setFirstPlace(data["ppg"][0]);
+                setSecondPlace(data["ppg"][1]);
+                setDataRow("ppg");
                 break;
             case "Playoffs Made":
                 setRankings(data["playoffs"]);
                 setFirstPlace(data["playoffs"][0]);
+                setSecondPlace(data["playoffs"][1]);
+                setDataRow("playoffs");
                 break;
             case "Finals Made":
                 setRankings(data["finals"]);
                 setFirstPlace(data["finals"][0]);
+                setSecondPlace(data["finals"][1]);
+                setDataRow("finals");
                 break;
             case "Playoff/Rate":
                 setRankings(data["playoff_rate"]);
                 setFirstPlace(data["playoff_rate"][0]);
+                setSecondPlace(data["playoff_rate"][1]);
+                setDataRow("playoff_rate");
                 break;
             default:
                 setRankings(data["titles"]);
                 setFirstPlace(data["titles"][0]);
+                setSecondPlace(data["titles"][1]);
+                setDataRow("titles");
         }
     }
 
@@ -84,7 +148,6 @@ export default function LeaderBoard(props) {
         "Finals Made",
         "Playoff/Rate",
     ]
-    // let leaderImage = "../../img/" + firstPlace["image"] TODO
 
     return (
         <div className="board-body">
@@ -99,11 +162,22 @@ export default function LeaderBoard(props) {
                 <section className="card-info card-section">
                     <i className="ion-navicon menu"></i>
                     <i className="ion-ios-search search"></i>
+                    <section className="statistics">
+
+                        <article className="statistic">
+                            <h4 className="statistic-title2">
+                                Category
+                            </h4>
+                            <h3 className="statistic-value">
+                                {firstPlace.category_type} {firstPlace.category}
+                            </h3>
+                        </article>
+
+                    </section>
                     <img
                         className={"leader-image"}
-                        // src={require("../../img/image1.png")}  // TODO
-                        alt="Team Image"/>
-
+                        src={require("../../img/firstPlace.png")}
+                        alt="First Place"/>
                     <section className="user row">
                         <div className="user-header">
                             {firstPlace.team_name}
@@ -131,16 +205,82 @@ export default function LeaderBoard(props) {
                                 {firstPlace.total}
                             </h3>
                         </article>
+
+                    </section>
+                    <section className="statistics">
+                        <article className="statistic">
+                            <h4 className="statistic-title">
+                                Year
+                            </h4>
+                            <h3 className="statistic-value">
+                                {firstPlace.season_start_year}
+                            </h3>
+                        </article>
+
+                        <article className="statistic">
+                            <h4 className="statistic-title">
+                                Active
+                            </h4>
+                            <h3 className="statistic-value">
+                                {(firstPlace.is_active) ? firstPlace.years_count + "+" : firstPlace.years_count}
+                            </h3>
+                        </article>
+
+                    </section>
+                    <img
+                        className={"leader-image"}
+                        src={require("../../img/secondPlace.png")}
+                        alt="Second Place"/>
+
+                    <section className="user row">
+                        <div className="user-header">
+                            {secondPlace.team_name}
+                            <h2 className="sub header">
+                                {secondPlace.name}
+                            </h2>
+                        </div>
                     </section>
 
-                    <div className="dial">
-                        <div className="dial-title">
-                            {firstPlace.category_type}
-                        </div>
-                        <div className="dial-value">
-                            {firstPlace.category}
-                        </div>
-                    </div>
+                    <section className="statistics">
+                        <article className="statistic">
+                            <h4 className="statistic-title">
+                                Rank
+                            </h4>
+                            <h3 className="statistic-value">
+                                #2
+                            </h3>
+                        </article>
+
+                        <article className="statistic">
+                            <h4 className="statistic-title">
+                                Score
+                            </h4>
+                            <h3 className="statistic-value">
+                                {secondPlace.total}
+                            </h3>
+                        </article>
+
+                    </section>
+                    <section className="statistics">
+                        <article className="statistic">
+                            <h4 className="statistic-title">
+                                Year
+                            </h4>
+                            <h3 className="statistic-value">
+                                {secondPlace.season_start_year}
+                            </h3>
+                        </article>
+
+                        <article className="statistic">
+                            <h4 className="statistic-title">
+                                Active
+                            </h4>
+                            <h3 className="statistic-value">
+                                {(secondPlace.is_active) ? secondPlace.years_count + "+" : secondPlace.years_count}
+                            </h3>
+                        </article>
+
+                    </section>
                 </section>
 
                 <section className="card-details card-section">
@@ -148,6 +288,9 @@ export default function LeaderBoard(props) {
                     <nav className="menu">
                         <article className="menu-item menu-item-active">
                             Team
+                                <button className="button button1" onClick={setActive}>
+                                    {(isActive) ? 'ACTIVE ONLY' : 'ALL TEAMS'}
+                                </button>
                         </article>
                         <article className="menu-item">
                             Value
--- react_apps/fantasy_tracker/src/components/home/Rank.js
+++ react_apps/fantasy_tracker/src/components/home/Rank.js
@@ -9,17 +9,22 @@ export default function Rank(props) {
     return (
         <div>
             <dt>
+                <dd>
+                    <div className="leaderboard-name">
+                        <div className="leader-name">{props.team_name}</div> ({props.name})
+                    </div>
+                    <div
+                        className="leaderboard-value">
+                        {props.total} <div style={{display: "inline", fontWeight: "lighter"}}> ({props.year})</div>
+                    </div>
+                </dd>
                 <article className="progress">
+
                     <section className="progress-bar"
                              style={{width: widthValue, backgroundColor: barColor}}></section>
                 </article>
             </dt>
-            <dd>
-                <div className="leaderboard-name"><div style={{fontWeight: 'bold'}}>{props.team_name}</div> ({props.name})</div>
-                <div
-                    className="leaderboard-value"><div style={{fontWeight: 'bold'}}>{props.total}</div> <div style={{fontWeight: 'lighter'}}>({props.year})</div>
-                </div>
-            </dd>
+
         </div>
     )
 }
